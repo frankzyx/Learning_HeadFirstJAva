c1ab78e947d45614aeddebfcc9564c1752d1df14
frankzyx <frank91zyx@gmail.com>
2016-04-23 12:06:23 -0400

    music

--- Ch11/MiniMusicApp.java
+++ Ch11/MiniMusicApp.java
@@ -19,14 +19,16 @@ public class MiniMusicApp {
 			
 			Track track = seq.createTrack();
 			
+			// 144 for note on and 128 for note off
+			// setMessage(on/off, channel, note, velocity);
 			ShortMessage a = new ShortMessage();
-			a.setMessage(144, 1, 44, 100);
+			a.setMessage(144, 1, 70, 100);
 			MidiEvent noteOn = new MidiEvent(a, 1);
 			track.add(noteOn);
 			
 			ShortMessage b = new ShortMessage();
 			b.setMessage(128, 1, 44, 100);
-			MidiEvent noteOff = new MidiEvent(b, 16);
+			MidiEvent noteOff = new MidiEvent(b, 10);
 			track.add(noteOff);
 			
 			player.setSequence(seq);
